11ca16d81aa9e2e010931da10e56ae39717e9af6
Axel Robbe <22393597+naveci@users.noreply.github.com>
2019-12-06 10:49:04 +0100

    Changed non-ascii character to minus-sign (17x)

--- Sooty.py
+++ Sooty.py
@@ -822,7 +822,7 @@ def analyzeEmail(email):
             if (len(req['details']['profiles']) != 0):
                 profileList = (req['details']['profiles'])
                 for each in profileList:
-                    print('   • %s' % each)
+                    print('   - %s' % each)
             else:
                 print('   No Profiles Found For This User')
 
@@ -930,21 +930,21 @@ def emailTemplateGen():
         print('\nThe sender has a reputation score of %s,' % req['reputation'], 'for the following reasons: ')
 
         if req['details']['spam']:
-            print(' • The sender has been reported for sending spam in the past.')
+            print(' - The sender has been reported for sending spam in the past.')
         if req['suspicious']:
-            print(' • It has been marked as suspicious on reputation checking websites.')
+            print(' - It has been marked as suspicious on reputation checking websites.')
         if req['details']['free_provider']:
-            print(' • The sender is using a free provider.')
+            print(' - The sender is using a free provider.')
         if req['details']['days_since_domain_creation'] < 365:
-            print(' • The domain is less than a year old.')
+            print(' - The domain is less than a year old.')
         if req['details']['blacklisted']:
-            print(' • It has been blacklisted on several sites.')
+            print(' - It has been blacklisted on several sites.')
         if req['details']['data_breach']:
-            print(' • Has been seen in data breaches')
+            print(' - Has been seen in data breaches')
         if req['details']['credentials_leaked']:
-            print(' • The credentials have been leaked for this address')
+            print(' - The credentials have been leaked for this address')
         if req['details']['malicious_activity']:
-            print(' • This sender has been flagged for malicious activity.')
+            print(' - This sender has been flagged for malicious activity.')
 
         malLink = 0     # Controller for mal link text
         for each in linksDict.values():
@@ -955,26 +955,26 @@ def emailTemplateGen():
             print('\nThe following potentially malicious links were found embedded in the body of the mail:')
             for key, value in linksDict.items():
                 if int(value) >= int(threshold):
-                    print(' • %s' % key)
+                    print(' - %s' % key)
 
         print('\nAs such, I would recommend the following: ')
 
         if 'suspicious' in rc:
-            print(' • Delete and Ignore the mail for the time being.')
+            print(' - Delete and Ignore the mail for the time being.')
 
         if 'malicious' in rc:
-            print(' • If you clicked any links or entered information into any displayed webpages let us know asap.')
+            print(' - If you clicked any links or entered information into any displayed webpages let us know asap.')
 
         if 'spam' in rc:
-            print(' • If you were not expecting the mail, please delete and ignore.')
-            print(' • We would advise you to use your email vendors spam function to block further mails.')
+            print(' - If you were not expecting the mail, please delete and ignore.')
+            print(' - We would advise you to use your email vendors spam function to block further mails.')
 
         if 'task' in rc:
-            print(' • If you completed any tasks asked of you, please let us know asap.')
-            print(' • If you were not expecting the mail, please delete and ignore.')
+            print(' - If you completed any tasks asked of you, please let us know asap.')
+            print(' - If you were not expecting the mail, please delete and ignore.')
 
         if 'benign' in rc:
-            print(' • If you were not expecting this mail, please delete and ignore.')
+            print(' - If you were not expecting this mail, please delete and ignore.')
             print('\nIf you receive further mails from this sender, you can use your mail vendors spam function to block further mails.')
 
         if 'suspicious' or 'malicious' or 'task' in rc:
