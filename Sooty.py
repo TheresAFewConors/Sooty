bde52e4cbac2a59ed90069373a87553386b05b69
TheresAFewConors <TheresAFewConors@Protonmail.com>
2020-01-28 15:44:50 +0000

    added missing contributions

--- Sooty.py
+++ Sooty.py
@@ -1040,7 +1040,7 @@ def contributors():
     print(" Eric Kelson for fixing pywin32 requirement not necessary on Linux systems in requirements.txt.")
     print(" Jenetiks for removing and tidying up duplicate imports that had accumulated over time.")
     print(" Nikosch86 for fixing an issue with Hexdigest not storing hashes correctly")
-    print(" Naveci for numerous bug fixes, QoL improvements, and Cisco Password 7 Decoding")
+    print(" Naveci for numerous bug fixes, QoL improvements, and Cisco Password 7 Decoding, and introduced a workflow to helps with issues in future. Phishtank support has now also been added.")
     print(" Paralax for fixing typos in the readme")
     extrasMenu()
 
