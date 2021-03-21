1d2d178cfa20c24bfd5e0f2a4b8cd33cc63ed295
TheresAFewConors <TheresAFewConors@Protonmail.com>
2021-03-21 17:49:46 +0000

    Update Sooty.py

--- Sooty.py
+++ Sooty.py
@@ -8,6 +8,8 @@
 """
 
 import base64
+import sys
+
 from unfurl import core
 import hashlib
 import html.parser
@@ -27,6 +29,7 @@ from Modules import iplists
 from Modules import phishtank
 from Modules import TitleOpen
 from datetime import datetime, date
+
 
 try:
     import win32com.client
@@ -160,6 +163,16 @@ def decodev2(rewrittenurl):
         if url not in linksFoundList:
             linksFoundList.append(url)
 
+def decodev3(rewrittenurl):
+    match = re.search(r'v3/__(?P<url>.+?)__;', rewrittenurl)
+    if match:
+        url = match.group('url')
+        if re.search(r'\*(\*.)?', url):
+            url = re.sub('\*', '+', url)
+            print(url)
+            if url not in linksFoundList:
+                linksFoundList.append(url)
+
 def titleLogo():
     TitleOpen.titleOpen()
     os.system('cls||clear')
@@ -212,17 +225,21 @@ def proofPointDecoder():
     print(" --------------------------------- ")
     rewrittenurl = str(input(" Enter ProofPoint Link: ").strip())
     match = re.search(r'https://urldefense.proofpoint.com/(v[0-9])/', rewrittenurl)
+    matchv3 = re.search(r'urldefense.com/(v3)/', rewrittenurl)
     if match:
         if match.group(1) == 'v1':
             decodev1(rewrittenurl)
             for each in linksFoundList:
                 print('\n Decoded Link: %s' % each)
-        elif match.group(1) == 'v2':
+        if match.group(1) == 'v2':
             decodev2(rewrittenurl)
             for each in linksFoundList:
                 print('\n Decoded Link: %s' % each)
-        else:
-            print('Unrecognized version in: ', rewrittenurl)
+
+    if matchv3.group(1) == 'v3':
+        decodev3(rewrittenurl)
+        for each in linksFoundList:
+            print('\n Decoded Link: %s' % each)
     else:
         print(' No valid URL found in input: ', rewrittenurl)
 
